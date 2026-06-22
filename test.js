2ebd91d94183ffb3c44d7b9846ea17fd2c820ec8
Varad Gotkhindikar <varad7611@gmail.com>
2026-06-22 20:56:47 +0530

    Update test.js

--- test.js
+++ test.js
@@ -2,6 +2,8 @@ const express = require('express');
 const http = require('http');
 const app = express();
 
+app.get('/', (req, res) => res.send('Hello World!'));
+
 const server = app.listen(5000, () => {
     console.log(`Server is running on port 5000`);
     
